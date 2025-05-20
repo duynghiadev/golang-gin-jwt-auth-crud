f020ca922a66ab0d3f67886da846e9d2346a37d1
duynghiadev <duynghia22302@gmail.com>
2025-05-20 08:12:27 +0700

    style: add empty line after fmt import for better readability

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/api/router"
 	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/config"
 	"github.com/RakibSiddiquee/golang-gin-jwt-auth-crud/db/initializers"
